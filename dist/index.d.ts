a3d77df944ba10b2c3329d7fc0137bdb65c572c3
next_hieu.vo <hieu.vo@nexpando.com>
2022-05-24 12:31:15 +0700

    remove unused

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,9 +1,7 @@
-import { ApplicationConfig, ComponentApplication } from './application';
 export * from './payment.component';
 export * from './keys';
 export * from './constant';
 export * from './utils';
 export * from './models';
 export * from './services';
-export * from './application';
-export declare function main(options?: ApplicationConfig): Promise<ComponentApplication>;
+export * from './typing.d';
--- dist/index.js
+++ dist/index.js
@@ -1,47 +1,11 @@
 "use strict";
-var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.main = void 0;
 const tslib_1 = require("tslib");
-const application_1 = require("./application");
 (0, tslib_1.__exportStar)(require("./payment.component"), exports);
 (0, tslib_1.__exportStar)(require("./keys"), exports);
 (0, tslib_1.__exportStar)(require("./constant"), exports);
 (0, tslib_1.__exportStar)(require("./utils"), exports);
 (0, tslib_1.__exportStar)(require("./models"), exports);
 (0, tslib_1.__exportStar)(require("./services"), exports);
-(0, tslib_1.__exportStar)(require("./application"), exports);
-async function main(options = {}) {
-    const app = new application_1.ComponentApplication(options);
-    await app.boot();
-    await app.start();
-    const url = app.restServer.url;
-    console.log(`Server is running at ${url}`);
-    console.log(`Try ${url}/ping`);
-    return app;
-}
-exports.main = main;
-if (require.main === module) {
-    // Run the application
-    const config = {
-        rest: {
-            port: +((_a = process.env.PORT) !== null && _a !== void 0 ? _a : 3000),
-            host: process.env.HOST,
-            // The `gracePeriodForClose` provides a graceful close for http/https
-            // servers with keep-alive clients. The default value is `Infinity`
-            // (don't force-close). If you want to immediately destroy all sockets
-            // upon stop, set its value to `0`.
-            // See https://www.npmjs.com/package/stoppable
-            gracePeriodForClose: 5000,
-            openApiSpec: {
-                // useful when used with OpenAPI-to-GraphQL to locate your application
-                setServersFromRequest: true,
-            },
-        },
-    };
-    main(config).catch(err => {
-        console.error('Cannot start the application.', err);
-        process.exit(1);
-    });
-}
+(0, tslib_1.__exportStar)(require("./typing.d"), exports);
 //# sourceMappingURL=index.js.map
